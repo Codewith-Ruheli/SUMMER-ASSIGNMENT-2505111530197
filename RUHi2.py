75ac1b516b6dc58b7918872dad110fcde7b8d834
Ruheli <ruhelighosh@gmail.com>
2026-06-04 20:41:07 +0530

    first program

---
+++ RUHi2.py
@@ -0,0 +1,10 @@
+
+#reverse a number
+num = int(input("enter a number:"))
+rev = 0
+while num > 0:
+    rem = num % 10
+    rev = rev * 10 + rem
+    num = num//10
+    print("reverse the number is:",rev)
+
