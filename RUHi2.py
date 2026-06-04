c9ff22bdf8ef2d45d00fafeb09fcb1eb317d1876
Ruheli <ruhelighosh@gmail.com>
2026-06-04 21:24:16 +0530

    first program

--- RUHi2.py
+++ RUHi2.py
@@ -1,9 +1,15 @@
 
-#find product of digits
-n = int(input("enter a number:"))
-product = 1
-while n > 0:
-    digit = n % 10
-    product = product * digit
-    n = n//10
-print("product of digits =",product)
+#check whether a number is palindrome or not
+num= int(input("enter a number: "))
+temp=num
+rev=0
+while temp>0:
+    digit=num%10
+    rev=rev*10+digit
+    temp=temp//10
+    if num ==rev:
+        print(num,"palindrome")
+    else:
+        print(num,"not palindrome")
+
+
