d724fe9c6848bcb455720727df29c9520aef620b
Ruheli <ruhelighosh@gmail.com>
2026-06-06 21:24:24 +0530

    day3

--- Day3.py
+++ Day3.py
@@ -1,11 +1,15 @@
 
-#find nth fibonacci term
-n=int(input("enter the term number: "))
-a=0
-b=1
-for i in range(n-1):
-    c=a+b
-    a=b
-    b=c
-    print(c)
+#to check armstrong number
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10 
+if num == sum:
+    print(num, "is an armstrong number")    
+else:
+    print(num, "is not an armstrong number")
+
     
