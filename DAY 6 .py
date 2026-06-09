563f541e619127d911374c00f7f35cc10d700334
Ruheli <ruhelighosh@gmail.com>
2026-06-09 21:49:03 +0530

    Day 6. Q1

---
+++ DAY 6 .py
@@ -0,0 +1,10 @@
+
+#recursive factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = int(input("Enter a number: "))
+print("Factorial of", n, "is", factorial(n))
