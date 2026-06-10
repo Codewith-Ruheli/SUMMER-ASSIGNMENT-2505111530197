047e8db84e836d88bff7e793b84d49223585f241
Ruheli <ruhelighosh@gmail.com>
2026-06-10 21:14:35 +0530

    Q2

--- Day 7.py
+++ Day 7.py
@@ -1,7 +1,11 @@
 
-#print half pyramid pattern
-n=int(input("enter number of rows:"))
+#print number of triangle
+
+n=5
 for i in range(1,n+1):
-    for j in range(i):
-        print("*",end="")
+    for j in range(1,i+1):
+        print(j,end="")
         print()
+
+
+    
