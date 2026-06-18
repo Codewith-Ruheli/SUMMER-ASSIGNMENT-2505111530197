c99357a5cca3fce1acae53c9830e983d9626d954
Ruheli <ruhelighosh@gmail.com>
2026-06-18 18:04:44 +0530

    DAY 13

--- Q4.py
+++ Q4.py
@@ -1,14 +1,6 @@
-#to count even and odd elements
-arr=[34,45,67,87,56]
-even=0
-odd=0
+#find duplicate in an array
+arr = [3,4,3,55,6,7]
 for i in arr:
-    if i % 2 == 0:
-        even += 1
-    else:
-        odd +=1
+    if arr.count(i)>1:
+        print(i)
         
-    print("even element=",even)
-    print("odd element=",odd)
-    
-
